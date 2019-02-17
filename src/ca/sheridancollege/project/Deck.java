820985ef4f050414868fd48986e6f4dc580314c2
Phan Nguyen Anh <phannguyenanhvn@gmail.com>
2019-02-17 18:14:15 -0500

    Added Deck class

---
+++ src/ca/sheridancollege/project/Deck.java
@@ -0,0 +1,28 @@
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Anh Phan
+ */
+public class Deck extends GroupOfCards{    
+    public Deck(int size){
+        super(size);
+        
+        for(int i = 0; i < 13; i++){
+            for (int j = 0; j < 4; j++){
+                super.showCards().add(new RegularCard(RegularCard.RANKS[i],RegularCard.SUITS[j]));
+            }
+        }
+                
+        super.shuffle();
+    }
+    
+    public RegularCard deal(){
+        return super.showCards().remove(0);
+    }
+    
+    public String toString(){
+        return super.showCards().toString();
+    }
+}
